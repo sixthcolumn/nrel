0bd79931b782abe922b7f077d3eb14aa653e6195
Ben Goodwin <benj.goodwin@gmail.com>
2016-06-21 18:57:30 -0400

    removed target, should not be here

--- 61968/target/generated-sources/cxf/com/epri/_2016/getdergroupstatuses/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.epri.com/2016/GetDERGroupStatuses#", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.epri._2016.getdergroupstatuses;
--- 61968/target/generated-sources/cxf/com/sixthc/cim/create/cxf/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://iec.ch/TC57/2015/MaintenanceOrdersMessage", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.sixthc.cim.create.cxf;
